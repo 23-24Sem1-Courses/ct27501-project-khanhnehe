e423fe530349150e92cd7cb4c9c6871d8ff57ffc
khanhnehe <103097409+khanhnehe@users.noreply.github.com>
2023-11-18 16:00:59 +0700

    hoàn thành giao diện cho website

--- view/about.php
+++ view/about.php
@@ -12,11 +12,12 @@
 <main class="container">
     <h3 id="title_about">GIỚI THIỆU</h3>
     <p>HK Book Storelà một cửa hàng sách trực tuyến nơi bạn có thể
-        khám phá và mua sắm các loại sách đa dạng từ các thể loại khác nhau.
+        khám phá và mua sắm các loại sách đa dạng từ các thể loại khác nhau .
     </p>
     <p>
         <strong>Danh Mục Sách Đa Dạng:</strong>
-        HK Book Store cung cấp một loạt các thể loại sách bao gồm tiểu thuyết, sách khoa học, sách kinh tế, nghệ thuật,
+        HK Book Store cung cấp một loạt các thể loại sách bao gồm sách Văn học, Truyẹn tranh comic, sách kinh tế, nghệ
+        thuật,
         và nhiều thể loại khác. Điều này giúp đáp ứng đa dạng nhu cầu đọc sách của người đọc.
     </p>
     <p> <strong>Mua Sắm Trực Tuyến:</strong>
