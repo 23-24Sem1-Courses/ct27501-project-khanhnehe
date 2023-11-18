20f00f38a5a056639dbd32506e37b487ae25316d
khanhnehe <103097409+khanhnehe@users.noreply.github.com>
2023-11-18 21:26:17 +0700

    hoàn thiện các chức năng admin

--- view/about.php
+++ view/about.php
@@ -24,4 +24,13 @@
 
         Với HK Book Store, bạn có thể thoải mái mua sắm sách từ nhà mình thông qua trang web của cửa hàng. Quá trình đặt
         hàng và thanh toán được thực hiện một cách thuận tiện, mang lại trải nghiệm mua sắm dễ dàng và nhanh chóng.</p>
+
+
+    <p> <strong> Trải Nghiệm Mua Sắm Dễ Dàng:</strong>
+        Chúng tôi cam kết mang đến cho bạn trải nghiệm mua sắm dễ dàng và thú vị. Từ những cuốn sách mới nhất đến những
+        tác phẩm kinh điển, HK Book Store sẽ là đối tác đáng tin cậy của bạn trong hành trình khám phá văn hóa sách. Hãy
+        ghé thăm cửa hàng trực tuyến của chúng tôi ngay hôm nay để bắt đầu hành trình đọc sách của bạn cùng HK Book
+        Store!
+    </p>
+
 </main>
